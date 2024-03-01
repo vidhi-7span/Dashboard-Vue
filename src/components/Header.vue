c370a00d7e9b1e0e398a6691655200cf39617e59
vidhi-7span <vidhi@7span.com>
2024-03-01 11:36:05 +0530

    Design header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div
+    class="bg-gray-200 h-20 flex justify-center items-center w-full shadow-md border-b border-gray-300"
+  >
     <p class="text-xl">Header</p>
   </div>
 </template>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-gray-200 w-72 h-screen">
+  <div
+    class="bg-gray-200 w-72 h-screen shadow-lg border-r border-gray-300 flex justify-center items-center"
+  >
     <p class="text-xl">Sidebar</p>
   </div>
 </template>
